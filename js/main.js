7da493b00773b9cad981fe350c47a58c7fea4ae9
Romona Blackwood <romonablackwood@Romonas-MacBook-Pro.local>
2017-08-15 17:06:00 -0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+var input = document.getElementById('input');
+
+var textarea = document.getElementById('textarea');
+
+var button = document.getElementById('button');
+
+var list = document.getElementById('list');
+//grab elements by id that are in html
+
+button.addEventListener('click', function() {
+console.log(input.value);
+console.log(textarea.value);
+
+var username = document.createElement('h1');
+var comment = document.createElement('p');
+var newComment = document.createElement('li');
+var removeButton = document.createElement('button');
+var buttonDiv = document.createElement('div');
+var showHide = document.createElement ('button');
+
+showHide.className = "btn btn-success";
+showHide.textContent = "Hide";
+
+removeButton.className = 'btn btn-danger';
+removeButton.textContent = 'Remove';
+
+//to put the text user generates into
+username.textContent = input.value;
+comment.textContent = textarea.value;
+
+//to clear text from from
+ input.value = "";
+ textarea.value= "";
+
+newComment.appendChild(username);
+newComment.appendChild(comment);
+list.appendChild(newComment);
+buttonDiv.appendChild(removeButton);
+list.appendChild(buttonDiv);
+buttonDiv.appendChild(showHide);
+
+removeButton.addEventListener('click',function(){
+  newComment.remove();
+  buttonDiv.remove();
+})
+showHide.addEventListener('click', function(){
+  if(newComment.style.display == 'none'){
+    newComment.style.display = 'block';
+    showHide.textContent = 'Hide'
+  } else {
+    showHide.textContent = 'Show';
+    newComment.style.display = 'none';
+  }
+})
+});
